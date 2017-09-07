86e65c747d34de7bfea614f05882dc575b15783c
jchadhowell <jchadhowell@gmail.com>
2017-09-07 17:17:53 -0500

    added state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import PropTypes from 'prop-types'
 
 class App extends React.Component {
 
+  constructor(){
+    super();
+
+    this.state = {
+      txt: "inital text"
+    }
+
+  }
+
   render(){
     let txt = this.props.txt;
     return (
       <div>
-        <h1>{txt}</h1>
-        <b>bold</b>
-        </div>
+        <h1>{this.state.txt}</h1>
+        <input type="text" onChange={this.update.bind(this)}/> 
+      </div>
     )
   }
+
+  update(e){
+    this.setState({txt:e.target.value});
+  }
+
+
 }
 
 App.propTypes = {
