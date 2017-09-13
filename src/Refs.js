20d6315035deb5417e79544e878433e80ef6a4ec
jchadhowell <jchadhowell@gmail.com>
2017-09-12 21:52:52 -0500

    basic refs

---
+++ src/Refs.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+class Refs extends React.Component {
+	constructor(){
+		super();
+		this.state = {a:''}
+	}
+
+	update(e){
+		this.setState(
+			{
+				a:this.refs.a.value,
+				b:this.refs.b.value
+			})
+	}
+
+	render(){
+		return (
+			<div>
+			<input 
+				ref="a"
+				type = "text"
+				onChange={this.update.bind(this)}
+			/>
+				{this.state.a}
+				<hr />
+				<input 
+				ref="b"
+				type = "text"
+				onChange={this.update.bind(this)}
+			/>
+				{this.state.b}
+			</div>
+
+			)
+
+
+	}
+
+
+
+}	
+
+export default Refs
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Refs from './Refs';
 
-ReactDOM.render(<App  num={2}/>, document.getElementById('root'));
+//ReactDOM.render(<App  num={2}/>, document.getElementById('root'));
+ReactDOM.render(<Refs/>, document.getElementById('root'));
